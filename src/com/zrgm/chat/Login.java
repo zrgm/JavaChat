cfca63b90637a8336f20c1ee4781a0f3e02deaaa
Zac Medeiros <zrgm01@gmail.com>
2020-05-29 12:11:14 -0500

    init + client GUI

---
+++ src/com/zrgm/chat/Login.java
@@ -0,0 +1,94 @@
+package com.zrgm.chat;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class Login extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	private JPanel contentPane;
+	private JTextField txtName;
+	private JTextField txtAddr;
+	private JLabel lblIpAddress;
+	private JLabel lblPort;
+	private JTextField txtPort;
+
+	
+	public Login() {
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		setResizable(false);
+		setTitle("Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300, 380);
+		setLocationRelativeTo(null);
+		//setBounds(100, 100, 300, 380);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtName = new JTextField();
+		txtName.setBounds(62, 59, 170, 31);
+		contentPane.add(txtName);
+		txtName.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Username:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setBounds(108, 34, 78, 20);
+		contentPane.add(lblNewLabel);
+		
+		txtAddr = new JTextField();
+		txtAddr.setColumns(10);
+		txtAddr.setBounds(62, 138, 170, 31);
+		contentPane.add(txtAddr);
+		
+		lblIpAddress = new JLabel("IP Address:");
+		lblIpAddress.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblIpAddress.setBounds(108, 113, 78, 20);
+		contentPane.add(lblIpAddress);
+		
+		lblPort = new JLabel("Port:");
+		lblPort.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblPort.setBounds(129, 194, 36, 20);
+		contentPane.add(lblPort);
+		
+		txtPort = new JTextField();
+		txtPort.setColumns(10);
+		txtPort.setBounds(62, 219, 170, 31);
+		contentPane.add(txtPort);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnNewButton.setBounds(102, 299, 89, 23);
+		contentPane.add(btnNewButton);
+	}
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
